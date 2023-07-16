12b6b43f1a411515d1aaa68851b872080636bb27
topoescobar <ivan90ge@gmail.com>
2023-07-16 02:07:58 -0300

    agregar descripciones

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const taskList = [
-  { id: 0, title: 'Practicar react hooks', description: ' ', done: true },
+  { id: 0, title: 'Practicar react hooks', description: 'Ejemplos de la documentacion oficial ', done: true },
   { id: 1, title: 'Estudiar nodejs y express', description: ' ', done: false },
   { id: 2, title: 'Estudiar persistencia con mongoDB', description: ' ', done: false },
-  { id: 3, title: 'Armar API', description: ' ', done: false },
+  { id: 3, title: 'Armar API', description: 'Revisar curso por mail de FCC', done: true },
   { id: 4, title: 'Conectar backend y frontend', description: ' ', done: false }
 ]
 
